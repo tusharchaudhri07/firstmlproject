c9a00f66e4c4194c19e58d95f6b28d7ea1ffe3c4
tc3158494-cpu <tc3158494@gmail.com>
2025-12-23 23:54:18 +0530

    hyperparameter turing

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import sys
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -15,15 +16,19 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train,y_train,x_test ,y_test,models):
+def evaluate_model(x_train,y_train,x_test ,y_test,models , param):
         try:
             report =  {} 
             for i in range(len(list(models))):
+                model = list(models.values())[i]
+                para=param[list(models.keys())[i]]
 
-                model=list(models.values())[i]
+                gs = GridSearchCV(model,para,cv=3)
+                gs.fit(X_train,y_train)
 
-                model.fit(x_train,y_train)
 
+                model.set_params(**gs.best_params_)
+                model.fit(X_train,y_train)
 
                 y_train_pred=model.predict(x_train)
 
